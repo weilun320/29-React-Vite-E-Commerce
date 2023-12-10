a03cc91a9182e3466f2f2aaf1df6a966224a9142
Wei Lun <41337787+weilun320@users.noreply.github.com>
2023-12-11 01:01:24 +0800

    Fix missing image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,21 +9,21 @@ const hotItem = (
     title={"Don't Miss It!"}
     items={[
       {
-        image: "../public/assets/hot-item-1.jpg",
+        image: "./assets/hot-item-1.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!",
         price: 160,
         discount: 0
       },
       {
-        image: "../public/assets/hot-item-2.jpg",
+        image: "./assets/hot-item-2.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!",
         price: 110,
         discount: 0
       },
       {
-        image: "../public/assets/hot-item-3.jpg",
+        image: "./assets/hot-item-3.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!",
         price: 180,
@@ -38,22 +38,22 @@ const newDrop = (
     title={"New Drop"}
     items={[
       {
-        image: "../public/assets/new-drop-1.jpg",
+        image: "./assets/new-drop-1.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!"
       },
       {
-        image: "../public/assets/new-drop-2.jpg",
+        image: "./assets/new-drop-2.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!"
       },
       {
-        image: "../public/assets/new-drop-3.jpg",
+        image: "./assets/new-drop-3.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!"
       },
       {
-        image: "../public/assets/new-drop-4.jpg",
+        image: "./assets/new-drop-4.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!"
       },
@@ -66,21 +66,21 @@ const valueBuy = (
     title={"Value Buy"}
     items={[
       {
-        image: "../public/assets/value-buy-1.jpg",
+        image: "./assets/value-buy-1.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!",
         price: 60,
         discount: 20
       },
       {
-        image: "../public/assets/value-buy-2.jpg",
+        image: "./assets/value-buy-2.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!",
         price: 150,
         discount: 20
       },
       {
-        image: "../public/assets/value-buy-3.jpg",
+        image: "./assets/value-buy-3.jpg",
         name: "Lorem ipsum",
         description: "Harum laborum ipsa labore recusandae eveniet aspernatur obcaecati dolores!",
         price: 90,
@@ -93,7 +93,7 @@ const valueBuy = (
 const styleBanner1 = (
   <Banner
     alt={"Styling-1"}
-    image={"../public/assets/styling-1.jpg"}
+    image={"./assets/styling-1.jpg"}
     title={"Want Something Special?"}
     link={"Check Out Our Exclusive"}
   />
@@ -102,7 +102,7 @@ const styleBanner1 = (
 const styleBanner2 = (
   <Banner
     alt={"Styling-2"}
-    image={"../public/assets/styling-2.jpg"}
+    image={"./assets/styling-2.jpg"}
     title={"Not Sure How To Style?"}
     link={"Click Here For More Styling"}
   />
@@ -111,7 +111,7 @@ const styleBanner2 = (
 const styleBanner3 = (
   <Banner
     alt={"Styling-3"}
-    image={"../public/assets/styling-3.jpg"}
+    image={"./assets/styling-3.jpg"}
     title={"Stay Connected"}
     link={"Subscribe To Newsletter"}
   />
